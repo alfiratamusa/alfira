825294f1b7092de4e9f59bfb35aade29a4d7ab8d
alfiratamusa1 <alfiraanggelatamusa@gmail.com>
2023-03-31 15:19:06 +0700

    Tugas proyek

---
+++ src/tiga.jsx
@@ -0,0 +1,24 @@
+funcation formatNama(user) {
+    return user.fristName + ' ' + user,lastName;
+}
+
+const user = {
+    fristName: 'Alfira',
+    lastName: 'Tamusa'
+}
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
+
+function Tiga() (
+    return (
+        element
+    );
+)
+
+
+export default Tiga;
